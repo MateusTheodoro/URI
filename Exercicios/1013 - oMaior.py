71030a870c14ae9f42ee0216148128c7a9144414
MateusTheodoro <mateus-jau@hotmail.com>
2021-04-27 12:25:31 -0300

    Update 1013 - oMaior.py

--- Exercicios/1013 - oMaior.py
+++ Exercicios/1013 - oMaior.py
@@ -2,4 +2,8 @@ numero = input().split(' ')
 a = int(numero[0])
 b = int(numero[1])
 c = int(numero[2])
-MaiorAB = (a)
+MaiorAB = (a+b+abs(a-b)) / 2
+if c > MaiorAB:
+    print(f'{c} eh o maior')
+else:
+    print(f'{MaiorAB:.0f} eh o maior')
