bea1afe3a12785e63cbc05e8e29241efe2c23255
MateusTheodoro <mateus-jau@hotmail.com>
2021-06-05 17:44:04 -0300

    Update 1015 - DistanciaEntrePontos.py

--- Exercicios/1015 - DistanciaEntrePontos.py
+++ Exercicios/1015 - DistanciaEntrePontos.py
@@ -1,11 +1,11 @@
 p1 = input().split(' ')
 x1 = float(p1[0])
-x2 = float(p1[1])
+y1 = float(p1[1])
 
 p2 = input().split(' ')
-y1 = float(p1[0])
+x2 = float(p2[0])
 y2 =float(p2[1])
 
-distancia = ( ( (pow(x2-x1,2)) +  (pow(y2-y1, 2)) ) / 2) ** 0.5
+distancia = ( pow( (x2-x1) ,2) + pow( (y2-y1) ,2) ) ** 0.5
 
 print(f'{distancia:.4f}')
