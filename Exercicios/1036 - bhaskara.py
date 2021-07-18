a4c13f207cb5059ca8768c0b1cb0ec34f5256c17
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-18 15:23:09 -0300

    Update 1036 - bhaskara.py

--- Exercicios/1036 - bhaskara.py
+++ Exercicios/1036 - bhaskara.py
@@ -3,3 +3,12 @@ a = float(num[0])
 b = float(num[1])
 c = float(num[2])
 
+delta = (b ** 2) - (4 * a * c)
+
+if delta < 0 or delta == 0 or a == 0:
+    print("Impossivel calcular")
+else:
+    r1 = (- b + (delta ** 0.5)) / (2 * a)
+    print(f'R1 = {r1:.5f}')
+    r2 = (- b - (delta ** 0.5)) / (2 * a)
+    print(f'R2 = {r2:.5f}')
