84aaae3b72f6f1223fc071b740bea79755afc759
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-25 19:58:24 -0300

    Update 1037 - intervalo.py

--- Exercicios/1037 - intervalo.py
+++ Exercicios/1037 - intervalo.py
@@ -1,4 +1,16 @@
 valor = float(input())
 
 if valor >= 0 and valor <= 25:
-    print(f'Intervalo [0,25)')
+    print(f'Intervalo [0,25]')
+
+elif valor > 25 and valor <= 50:
+    print(f'Intervalo (25,50]')
+
+elif valor > 50 and valor <= 75:
+    print(f'Intervalo (50,75]')
+
+elif valor > 75 and valor <= 100:
+    print(f'Intervalo (75,100]')
+
+else:
+    print(f'Fora de intervalo')
