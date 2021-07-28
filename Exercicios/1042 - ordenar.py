0cbdddd459af3ce4fc59e081381f6ed3a4b6c391
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-28 01:01:45 -0300

    Update 1042 - ordenar.py

--- Exercicios/1042 - ordenar.py
+++ Exercicios/1042 - ordenar.py
@@ -2,3 +2,29 @@ valor = input().split(' ')
 a = int(valor[0])
 b = int(valor[1])
 c = int(valor[2])
+
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+elif c > a and c > b:
+    print(c)
+
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+elif c > a and c > b:
+    print(c)
+
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+elif c > a and c > b:
+    print(c)
+
+
+
+print()
+print(f'{a}\n{b}\n{c}')
