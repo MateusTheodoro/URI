1a4cd2e95f861a2df08038ae8027a68129aefa31
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-07-31 14:01:05 -0300

    Create 1052 - mes.py

---
+++ Exercicios/1052 - mes.py
@@ -0,0 +1,5 @@
+nro_mes = int(input())
+mes = ['January', 'February', 'March', 'April', 'May', 'June',
+       'July', 'August', 'September', 'October', 'November', 'December']
+
+print(mes[nro_mes - 1])
