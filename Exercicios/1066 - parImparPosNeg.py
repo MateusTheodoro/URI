3e84caa611c72756308da8ea19b7971af1cac249
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-08-01 12:41:22 -0300

    Update 1066 - parImparPosNeg.py

--- Exercicios/1066 - parImparPosNeg.py
+++ Exercicios/1066 - parImparPosNeg.py
@@ -2,19 +2,23 @@ contador = 1
 negativo = 0
 positivo = 0
 pares = 0
+impares = 0
 
 while contador <= 5:
 
     nro = int(input())
 
+    if nro % 2 == 0:
+        pares += 1
+    if nro % 2 != 0:
+        impares += 1
+    if nro > 0:
+        positivo += 1
     if nro < 0:
         negativo += 1
-    elif nro > 0:
-        positivo += 1
-    elif nro % 2 == 0:
-        pares += 1
     contador += 1
 
-print(negativo)
-print(positivo)
-print(pares)
+print(f'{pares} valor(es) par(es)')
+print(f'{impares} valor(es) impar(es)')
+print(f'{positivo} valor(es) positivo(s)')
+print(f'{negativo} valor(es) negativo(s)')
