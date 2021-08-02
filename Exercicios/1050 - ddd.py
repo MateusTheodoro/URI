8e24e6399c5e23cef62b55243901e023659bccaa
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-08-02 20:27:24 -0300

    Create 1050 - ddd.py

---
+++ Exercicios/1050 - ddd.py
@@ -0,0 +1,21 @@
+ddd = int(input())
+cidades = ['Brasilia', 'Salvador', 'Sao Paulo', 'Rio de Janeiro', 'Juiz de Fora', 'Campinas', 'Vitoria',
+           'Belo Horizonte']
+if ddd == 61:
+    print(f'{cidades[0]}')
+elif ddd == 71:
+    print(f'{cidades[1]}')
+elif ddd == 11:
+    print(f'{cidades[2]}')
+elif ddd == 21:
+    print(f'{cidades[3]}')
+elif ddd == 32:
+    print(f'{cidades[4]}')
+elif ddd == 19:
+    print(f'{cidades[5]}')
+elif ddd == 27:
+    print(f'{cidades[6]}')
+elif ddd == 31:
+    print(f'{cidades[7]}')
+else:
+    print(f'DDD nao cadastrado')
