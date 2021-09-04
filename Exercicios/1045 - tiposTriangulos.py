1fb6f7abdcc44f2ffa1b127714b89334515845a2
MateusTheodoro <61423297+MateusTheodoro@users.noreply.github.com>
2021-09-04 18:40:00 -0300

    Update 1045 - tiposTriangulos.py

--- Exercicios/1045 - tiposTriangulos.py
+++ Exercicios/1045 - tiposTriangulos.py
@@ -1,22 +1,26 @@
 valores = input().split(' ')
-a = float(valores[0])
-b = float(valores[1])
-c = float(valores[2])
-
-if a >= (b + c):
+d1 = float(valores[0])
+d2 = float(valores[1])
+d3 = float(valores[2])
+decrescente = [d1, d2, d3]
+novo = sorted(decrescente, key=float, reverse=True)
+a = novo[0]
+b = novo[1]
+c = novo[2]
+if a >= b + c:
     print('NAO FORMA TRIANGULO')
+else:
+    if (a ** 2) == (b ** 2) + (c ** 2):
+        print('TRIANGULO RETANGULO')
 
-if (a ** 2) == (b ** 2) + (c ** 2):
-    print('TRIANGULO RETANGULO')
+    if (a ** 2) > (b ** 2) + (c ** 2):
+        print('TRIANGULO OBTUSANGULO')
 
-if (a ** 2) > (b ** 2 + c ** 2):
-    print('TRIANGULO OBTUSANGULO')
+    if (a ** 2) < (b ** 2) + (c ** 2):
+        print('TRIANGULO ACUTANGULO')
 
-if (a ** 2) < (b ** 2 + c ** 2):
-    print('TRIANGULO ACUTANGULO')
+    if a == b == c:
+        print('TRIANGULO EQUILATERO')
 
-if a == b == c:
-    print('TRIANGULO EQUILATERO')
-
-if a == b != c or a == c != b or b == a != c or b == c != a or c == b != a or c == a != b:
-    print('TRIANGULO ISOSCELES')
+    if a == b != c or a == c != b or b == a != c or b == c != a or c == b != a or c == a != b:
+        print('TRIANGULO ISOSCELES')
